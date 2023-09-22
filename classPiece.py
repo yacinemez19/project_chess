128fe84ce97f0499a8a7e91edd97c217d66f2ef5
yacinemez19 <135065821+yacinemez19@users.noreply.github.com>
2023-09-22 11:26:08 +0200

    Update classPiece.py

--- classPiece.py
+++ classPiece.py
@@ -1,3 +1,10 @@
 class piece:
-    def __init__(self, position) -> None:
+    def __init__(self, position, couleur) -> None:
         self.position = position #prend None si la piece n'est pas sur le plateau
+        self.couleur = couleur
+
+    def recuperer_nom(self):
+        pass
+
+    def vect_deplacement(self):
+        pass
