16112e0851cbcfd3a1a9ae2c6cf82c4dc35add2b
yacinemez19 <135065821+yacinemez19@users.noreply.github.com>
2023-10-03 09:44:22 +0200

    ajout de la class tour

--- Tour.py
+++ Tour.py
@@ -1,4 +1,17 @@
 from Piece import *
 from EtatEchecs import *
 from Echecs import *
-#bite
+
+class Pion(Piece):
+    def __init__(self, position, est_blanc) -> None:
+        super().__init__(position, est_blanc)
+        self.nom = 'T' if est_blanc else 't'
+
+    def coups_possibles(self, etat) -> list:
+        x = self.position[0]
+        y = self.position[1]
+        coups = []
+
+        for i,j in [(1,0), (-1, 0), (0, 1), (0, -1)]:
+            while 0 <= x <= 7 and 0 <= y <= 7 and not (x, y) in etat.plateau:
+                coups.append(x+i, y+j)
