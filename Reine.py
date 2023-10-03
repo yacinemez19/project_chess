d65f3222865ca5d1d0f8dddb956759d69f6fb32b
LDylan200 <dylanledu0615@gmail.com>
2023-10-03 09:48:22 +0200

    classe reine créée
    pareil que fou et tour mais combiné

---
+++ Reine.py
@@ -0,0 +1,18 @@
+from Piece import *
+from EtatJeu import *
+from Jeu import *
+
+
+class Fou(Piece):
+    def __init__(self, position, est_blanc) -> None:
+        super().__init__(position, est_blanc)
+        self.nom = 'F' if est_blanc else 'f'
+
+    def coups_possibles(self, etat) -> list:
+        x = self.position[0]
+        y = self.position[1]
+        coups = []
+        
+        for i, j in [(1,1),(-1,1),(-1,-1),(1,-1),(1,0),(0,1),(-1,0),(0,-1)]:
+            while 0 <= x+i <= 7 or 0 <= y+j <= 7 and not (x,y) in etat.plateau:
+                coups.append(x + i,y + j)
