37a8a4f5cbcf22e4547ed946ec11fc8c79b918c7
Erratu <145753829+Erratu@users.noreply.github.com>
2023-10-07 21:40:43 +0200

    Update Echecs.py

--- Echecs.py
+++ Echecs.py
@@ -11,15 +11,16 @@ import copy
 class Echecs(Jeu) : 
     def traduire(self, mouvement) : 
       colonnes =  {'a' : 0, 'b' : 1, 'c' : 2, 'd' : 3, 'e' : 4, 'f' : 5, 'g' : 6, 'h' : 7}
-      mouv_str = [x.strip() for x in mouvement.split()]
+      mouv_str = [x.strip() for x in mouvement.split('')]
+      
       try: 
         if mouv_str[0] in colonnes : 
           position1 = (colonnes[mouv_str[0]],int(mouv_str[1]))
-          position2 = (colonnes[mouv_str[2]],int(mouv_str[3]))
+          position2 = (colonnes[mouv_str[3]],int(mouv_str[4]))
           assert int(mouv_str[1]) < 8 and int(mouv_str[3]) < 8
         else :  
           position1 = (colonnes[mouv_str[1]],int(mouv_str[2]))
-          position2 = (colonnes[mouv_str[3]],int(mouv_str[4]))
+          position2 = (colonnes[mouv_str[4]],int(mouv_str[5]))
           assert int(mouv_str[2]) < 8 and int(mouv_str[4]) < 8    
       except: 
         return "Votre mouvement n'est pas valide. Veuillez respecter le format : type a6-b3 pour un mouvement et type Ca6-b3 pour une capture. Pour plus d'informations sur le format, appeler help"   
