e79aee6be37722c8ad820cf4af93e89c65458ce1
LDylan200 <dylanledu0615@gmail.com>
2023-10-08 21:00:40 +0200

    Update test_Pieces.py

--- test_Pieces.py
+++ test_Pieces.py
@@ -3,6 +3,7 @@ import pytest
 from Echecs import Echecs
 from EtatEchecs import EtatEchecs
 from Roi import Roi
+from Fou import Fou
 
 
 def test_coups_possibles():
@@ -12,5 +13,10 @@ def test_coups_possibles():
     for piece in etat.plateau.values():
         if piece.est_blanc and type(piece) == Roi:
             roi = piece
-            
     assert roi.coups_possibles(etat) == {(3,1)}
+    
+    fou = None
+    for piece in etat.plateau.values():
+        if piece.est_blanc and type(piece) == Fou:
+            fou = piece
+    assert fou.coups_possibles(etat) == {}
