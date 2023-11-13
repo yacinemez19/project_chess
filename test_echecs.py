da83d6da1257495514b23ba8ea9bec6d1e8f3665
LDylan200 <dylanledu0615@gmail.com>
2023-11-13 23:02:59 +0100

    tests échecs

--- test_echecs.py
+++ test_echecs.py
@@ -2,13 +2,12 @@ import pytest
 
 from Echecs import Echecs
 from EtatEchecs import EtatEchecs
+from Echecs import MovementImpossibleError
 
 def test_traduire():
     jeu = Echecs()
     mouvement_valide = "a2-b3"
-    assert jeu.traduire(mouvement) == [(0, 1), (1, 2)]
-    mouvement_invalide = "f4-i5"
-    assert jeu.traduire(mouvement_invalide) == "Votre mouvement n'est pas valide. Veuillez respecter le format..."
-
-def test_deplacer():
+    assert jeu.traduire(mouvement_valide) == [(0, 1), (1, 2)]
+    mouvement_invalide = "z4-i9"
+    assert jeu.traduire(mouvement_invalide) == "Votre mouvement n'est pas valide. Veuillez respecter le format : type a6-b3 pour un mouvement et type Ca6-b3 pour une capture, en respectant la taille 8x8 du plateau. Pour plus d'informations sur le format, appeler help"
     
