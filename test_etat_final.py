dbfd02b5a1e408c846dfd416e1128666492d7757
LDylan200 <dylanledu0615@gmail.com>
2023-11-16 15:03:40 +0100

    Update test_etat_final.py

--- test_etat_final.py
+++ test_etat_final.py
@@ -32,23 +32,3 @@ def test_insuffisances_materielles(e) :
     etat = e.charger("tests_etat_final\RvsR+F.txt")
     assert(e.etat_final(etat,[])[0])==True
            
-def test_3_repetitions(e) :
-    etat = e.charger("tests_etat_final\test_3_repetitions.txt")
-    historique = []
-    for i in range(3) : 
-        mouv = [(1,5),(0,6)]
-        e.deplacer(etat, mouv)
-        piece_jouee = etat.plateau[mouv[1]]
-        historique.append(mouv, piece_jouee)
-        
-        mouv = [(1,7),(0,7)]
-        e.deplacer(etat, mouv)
-        piece_jouee = etat.plateau[mouv[1]]
-        historique.append(mouv, piece_jouee)
-        
-        mouv = [(0,6),(1,5)]
-        e.deplacer(etat, mouv)
-        piece_jouee = etat.plateau[mouv[1]]
-        historique.append(mouv, piece_jouee)
-        
-    assert e.etat_final(etat, historique)[0] ==  True
